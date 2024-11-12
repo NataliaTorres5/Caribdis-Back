f05794250785d431eb2fb61f81a866898a2c0bec
NataliaTorres5 <nataliatorres88@gmail.com>
2024-11-11 21:48:35 -0500

    error conexion con mongo ya solucionado

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+
+
+
 dotenv.config()
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_CONNECT)
-    await mongoose.connect(process.env.MONGO_CONNECT);
+    console.log(process.env.MONGO_CONNECT, "esta conectando con base de datos o no")
+    await mongoose.connect(process.env.MONGO_CONNECT, {
+      serverSelectionTimeoutMS:30000 //30seconds timeout
+    });
     console.log('MongoDB connected')
   } catch (error) {
     console.error('Error connecting to MongoDB:', error)
@@ -14,3 +19,4 @@ const connectDB = async () => {
 };
 
 export default connectDB
+
