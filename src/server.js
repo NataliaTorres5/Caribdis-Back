52b7329ca7bd1e77b6d4db380616b7132ab9be4f
NataliaTorres5 <nataliatorres88@gmail.com>
2024-11-16 12:15:12 -0500

    testing cors

--- src/server.js
+++ src/server.js
@@ -5,25 +5,32 @@ import indexRouter from "./routes/indexRouter.js";
 import logger from "./middlewares/logger.js";
 import connectDB from "./config/database.js";
 
-dotenv.config();
 const server = express();
+dotenv.config();
 server.use(express.json());
-const allowedOrigins = ['http://localhost:5173']
+server.use(cors());
+
+server.use("/api", logger, indexRouter);
+
+connectDB();
+
+server.get("/", (request, response) => response.send("hola"));
+
+server.listen(process.env.PORT, () =>
+  console.log(`server is being listening in port ${process.env.PORT} `)
+);
+
+
+/*const allowedOrigins = ['http://localhost:5173,']
+
 server.use(cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) { const msg = 'The CORS policy for this site does not allow access from the specified Origin.'; return callback(new Error(msg), false); }
     return callback(null, true)
   },
-  methods: 'GET,POST,PUT,DELETE',
+  methods: 'GET,POST,PUT,DELETE, OPTIONS',
   allowedHeaders: 'Content-Type,Authorization'
 }));
 
-server.use("/api", logger, indexRouter);
-
-connectDB();
-server.get("/", (request, response) => response.send("hola"));
-
-server.listen(process.env.PORT, () =>
-  console.log(`server is being listening in port ${process.env.PORT} `)
-);
+*/ 
