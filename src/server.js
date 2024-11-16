5bd0fefe3e92e1cf8caabf1c80c4dbf389641ff7
NataliaTorres5 <nataliatorres88@gmail.com>
2024-11-15 23:12:03 -0500

    corrijo error

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const server = express();
 server.use(express.json());
 const allowedOrigins = ['http://localhost:5173']
 server.use(cors({
-  origin: function(origin, callbak) {
+  origin: function(origin, callback) {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) { const msg = 'The CORS policy for this site does not allow access from the specified Origin.'; return callback(new Error(msg), false); }
     return callback(null, true)
